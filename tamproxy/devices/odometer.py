67d019c2fb10141c5ab77700dec4650d3fe41ee7
Eric Wieser <wieser.eric@gmail.com>
2016-01-19 01:32:44 -0500

    Use new base class

--- tamproxy/devices/odometer.py
+++ tamproxy/devices/odometer.py
@@ -1,27 +1,40 @@
 from .device import Device, ContinuousReadDevice
 from .. import config as c
 
-class Odometer(Device):
+import struct
+
+class Odometer(ContinuousReadDevice):
 
     DEVICE_CODE =   c.devices.odometer.code
-    WRITE_CODE =    c.devices.odometer.read_code
+    READ_CODE =    c.devices.odometer.read_code
 
     def __init__(self, tamproxy, left_enc, right_enc, gyro, alpha):
         self.left_enc = left_enc
 		self.right_enc = right_enc
 		self.gyro = gyro
 		self.alpha = alpha
-		self.val = None
-        super(Odometer, self).__init__(tamproxy)
-        while self.id is None: pass
+		self.val = 0
+        super(Odometer, self).__init__(tamproxy, continuous=False)
+
+    def __repr__(self):
+        return super(Odometer, self).__repr__(
+            self.left_enc,
+            self.right_enc,
+            self.gyro,
+            alpha=self.alpha)
 
     @property
     def add_payload(self):
-        return self.DEVICE_CODE + chr(self.left_enc) + chr(self.right_enc) + chr(self.gyro) + chr(self.alpha)
+        return self.DEVICE_CODE + (
+            chr(self.left_enc.id) +
+            chr(self.right_enc.id) +
+            chr(self.gyro.id) +
+            chr(self.alpha.id)
+        )
 
     def read(self):
         self.tamp.send_request(self.id,
                                self.READ_CODE, self.handle_request)
-							   
-	def handle_reading(self, request, response):
-		self.val = response
+
+	def _handle_update(self, request, response):
+        self.val = struct.unpack('!f', response)[0]
