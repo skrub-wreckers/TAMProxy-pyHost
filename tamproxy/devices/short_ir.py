d6c3fb20b58e0d3e45d5dce9b242e9024123877c
Eric Wieser <wieser.eric@gmail.com>
2016-01-21 16:56:06 -0500

    Remove duplicated code from base classes

--- tamproxy/devices/short_ir.py
+++ tamproxy/devices/short_ir.py
@@ -3,16 +3,4 @@ from .. import config as c
 import .analog_input import AnalogInput
 
 class ShortIR(AnalogInput):
-
-    DEVICE_CODE =   c.devices.ir.code
-    READ_CODE =     c.devices.ir.read_code
-
-    def __init__(self, tamproxy, pin, continuous=False):
-        self.pin = pin
-        self.val = 0
-        self.dist = 0
-        super(AnalogInput, self).__init__(tamproxy, continuous)
-
-    def _handle_update(self, request, response):
-        self.val = (ord(response[0])<<8) + ord(response[1])
-        # Have some conversion in here for doing moving average to distance
+    pass
--- tamproxy/devices/ultrashort_ir.py
+++ tamproxy/devices/ultrashort_ir.py
@@ -3,16 +3,5 @@ from .. import config as c
 from .digital_input import DigitalInput
 
 class UltraShortIR(DigitalInput):
-
-    DEVICE_CODE =   c.devices.digital_input.code
-    READ_CODE =     c.devices.digital_input.read_code
-
-    def __init__(self, tamproxy, pin, pullup=True, continuous=False):
-        self.pin = pin
-        self.pullup = pullup
-        self.val = 0
-        self.prev_val = None
-        super(DigitalInput, self).__init__(tamproxy, continuous)
-
     def isClose(self):
         return self.val
